122866422aeaaa7e14884cdc1c2836d0420e308b
Derrick Nyasio <dnyasio2014@gmail.com>
2022-08-13 00:15:12 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+
+dodger.style.backgroundColor = 'red';
+
+//dodger.style.bottom = '30px';
+
+function moveDodgerLeft() {
+    const leftValue = dodger.style.left.replace("px", "");
+    const left = parseInt(leftValue, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const leftValue = dodger.style.left.replace("px", "");
+    const left = parseInt(leftValue, 10);
+
+    if (left < 400) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+dodger.addEventListener('keydown', function(event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+
+});
